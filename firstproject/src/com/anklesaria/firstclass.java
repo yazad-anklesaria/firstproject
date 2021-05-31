817209994951a309b698ca373910ae4d7685cb61
yazad <yazad@172.20.10.5>
2021-05-31 13:08:22 +0530

    Added in-code tutorial

--- firstproject/src/com/anklesaria/firstclass.java
+++ firstproject/src/com/anklesaria/firstclass.java
@@ -4,7 +4,27 @@ public class firstclass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		
+		/*
+		 *	Justin, this is the ToDo list:
+		 *	1) Declare integer, string and boolean variables
+		 *	2) Write a addition and subtraction calculator
+		*/
 
+		// I am declaring sample variables here
+		// Notice how I have added short forms of the variable type within the variable name. We declare variables
+		// using this method so that it is easier to understand by reading the variable name.
+		String strName;
+		strName = "Yazad";
+		
+		int intMyAge;
+		intMyAge = 42;
+		
+		boolean blnAlwaysDaddyNu;
+		blnAlwaysDaddyNu = true;
+		// End of tutorial
+		
+		//TODO: Add your code below this line
 	}
 
 }
